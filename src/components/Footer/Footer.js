8f3459eaac450a2f7296faa4547062b21670f581
Nguyen Hoang <n3verland0211@gmail.com>
2022-01-21 11:23:41 +0200

    finish home section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,6 @@
+import { ImLinkedin } from "react-icons/im";
+import { FaGithubSquare, FaCodepen } from "react-icons/fa";
+
 const Footer = () => {
   return <footer>Footer</footer>;
 };
--- src/components/MainSections/Home/Home.js
+++ src/components/MainSections/Home/Home.js
@@ -2,9 +2,30 @@ import classes from "./Home.module.css";
 
 const Home = () => {
   return (
-    <section className={classes["welcome-section"]} id="home">
-      <h1>Hello, I'm Nguyen Hoang</h1>
-      <p>Some content</p>
+    <section className={classes["home"]} id="home">
+      <div className={classes["intro-wrapper"]}>
+        <div className={classes["dots-wrapper"]}>
+          <div className={classes["dot-yellow"]}></div>
+          <div className={classes["dot-green"]}></div>
+          <div className={classes["dot-red"]}></div>
+        </div>
+
+        <div className={classes["content-wrapper"]}>
+          <div className={classes["left-column"]}>
+            <h1>
+              Hi, I'm <br />
+              <span className={classes["name"]}>Nguyen Hoang</span> <br />
+              Front End Dev
+            </h1>
+            <a href="#contact">Contact Me</a>
+          </div>
+          <div className={classes["right-column"]}>
+            <div className={classes["profile-picture"]}>
+              <div className={classes["profile-picture-bg"]}></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
