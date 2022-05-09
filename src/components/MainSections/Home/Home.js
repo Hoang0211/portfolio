fa3a1ee84f453bc3973c2349e1f36e977a7861c3
Nguyen Hoang <n3verland0211@gmail.com>
2022-05-09 11:04:18 +0300

    fix className and style for Home section

--- src/components/MainSections/Home/Home.js
+++ src/components/MainSections/Home/Home.js
@@ -5,39 +5,39 @@ import classes from './Home.module.css';
 const Home = () => {
   return (
     <section className={classes['home']} id='home'>
-      <div className={classes['home__wrapper']}>
-        <div className={classes['home__dots']}>
+      <div className={classes['wrapper']}>
+        <div className={classes['dots']}>
           <div className={`${classes['dot']} ${classes['dot-red']}`}></div>
           <div className={`${classes['dot']} ${classes['dot-yellow']}`}></div>
           <div className={`${classes['dot']} ${classes['dot-green']}`}></div>
         </div>
 
-        <div className={classes['home__content']}>
-          <article className={classes['home__column-left']}>
-            <h1 className={classes['home__title']}>
+        <div className={classes['content']}>
+          <article className={classes['column-left']}>
+            <h1 className={classes['title']}>
               Hi, I'm <br />
-              <span className={classes['home__name']}>Nguyen Hoang</span> <br />
+              <span className={classes['name']}>Nguyen Hoang</span> <br />
               Front End Dev
             </h1>
             <a
-              className={`${classes['btn']} ${classes['home__contact']}`}
+              className={`${classes['btn']} ${classes['btn-contact']}`}
               href='#contact'
             >
               Contact Me
             </a>
           </article>
-          <article className={classes['home__column-right']}>
-            <div className={classes['home__profile']}>
-              <div className={classes['home__profile-bg']}></div>
+          <article className={classes['column-right']}>
+            <div className={classes['profile']}>
+              <div className={classes['profile-bg']}></div>
             </div>
-            <div className={classes['home__socials']}>
+            <div className={classes['socials']}>
               <a
                 href='https://www.linkedin.com/in/hoang-nguyen-8b6733228/'
                 target='_blank'
                 rel='noreferrer'
               >
                 <FaLinkedinIn
-                  className={`${classes['home__icon']} ${classes['home__socials-linkedin']}`}
+                  className={`${classes['icon']} ${classes['linkedin']}`}
                 />
               </a>
               <a
@@ -46,7 +46,7 @@ const Home = () => {
                 rel='noreferrer'
               >
                 <FaGithub
-                  className={`${classes['home__icon']} ${classes['home__socials-github']}`}
+                  className={`${classes['icon']} ${classes['github']}`}
                 />
               </a>
               <a
@@ -55,7 +55,7 @@ const Home = () => {
                 rel='noreferrer'
               >
                 <FaCodepen
-                  className={`${classes['home__icon']} ${classes['home__socials-codepen']}`}
+                  className={`${classes['icon']} ${classes['codepen']}`}
                 />
               </a>
             </div>
