038a97f960eb5b3444c33d35ce3c62065d33bf1f
Nguyen Hoang <n3verland0211@gmail.com>
2022-05-09 10:59:40 +0300

    fix className and style for About section

--- src/components/MainSections/About/About.js
+++ src/components/MainSections/About/About.js
@@ -1,36 +1,36 @@
-import CV from "../../../assets/Nguyen-Viet-Hoang-CV.pdf";
+import CV from '../../../assets/Nguyen-Viet-Hoang-CV.pdf';
 
-import classes from "./About.module.css";
+import classes from './About.module.css';
 
 const About = () => {
   return (
-    <section className={classes["about"]} id="about">
-      <h1 className={classes["title"]}>About</h1>
-      <div className={classes["about-container"]}>
-        <div className={classes["left-column"]}>
-          <div className={classes["about-picture-bg"]}></div>
-        </div>
-        <div className={classes["right-column"]}>
-          <h2 className={classes["subtitle"]}>Personal Statement</h2>
-          <p className={classes["statement"]}>
-            A hardworking, motivated student with a strong background in front
-            end development. I am a third year IT student studying in
-            South-Eastern Finland University of Applied Sciences, and I am
-            looking for opportunities to gain more experience as a developer.
+    <section className={classes['about']} id='about'>
+      <h2 className={classes['title']}>About</h2>
+      <div className={classes['content']}>
+        <article className={classes['column-left']}>
+          <div className={classes['picture-bg']}></div>
+        </article>
+        <article className={classes['column-right']}>
+          <h3 className={classes['subtitle']}>Personal Statement</h3>
+          <p className={classes['statement']}>
+            I am a motivated, hardworking software developer with a strong
+            background in front-end development. Rightnow, I am a third year
+            student at South-Eastern University of Applied Sciences and looking
+            for opportunities to gain more experience as a web developer.
           </p>
-          <h2 className={classes["subtitle"]}>Skills</h2>
+          <h3 className={classes['subtitle']}>Skills</h3>
           <p>
-            A front end developer with primary focus on React:{" "}
-            <a target="_blank" href={CV} rel="noreferrer">
+            A front end developer with primary focus on React:
+            <a target='_blank' href={CV} rel='noreferrer'>
               Download Resume
             </a>
           </p>
-          <div className={classes["skills-container"]}>
+          <div className={classes['skills']}>
             <ul>
-              <li>HTML/CSS</li>
-              <li>JavaScript</li>
-              <li>JQuery</li>
-              <li>React</li>
+              <li>HTML/CSS/JS</li>
+              <li>CSS Module/SCSS</li>
+              <li>Typescript</li>
+              <li>ReactJS</li>
               <li>Redux</li>
             </ul>
 
@@ -42,7 +42,7 @@ const About = () => {
               <li>Git</li>
             </ul>
           </div>
-        </div>
+        </article>
       </div>
     </section>
   );
